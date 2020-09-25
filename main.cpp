768742cf615bdb2f6d763dddd0fecb013a0df3e1
Thakee Nathees <thakeenathees@gmail.com>
2020-09-25 19:39:56 +0530

    switcalble index implemented

--- main.cpp
+++ main.cpp
@@ -18,9 +18,18 @@
 
 #define INPUT(m_type, m_name) m_type m_name; std::cin >> m_name
 #define GETLINE(m_name) std::getline(std::cin, m_name)
+#define INPUT_IARR(m_arr, m_size) std::vector<int> m_arr; for (int _i = 0; _i < m_size; _i++) { INPUT(int, inp); m_arr.push_back(inp); }
 
 #define PRINT(m_what)  std::cout << (m_what)
 #define PRINTLN(m_what) std::cout << (m_what) << std::endl
+
+struct SwitchIndex {
+	int _ind = 0;
+	inline int ind() const { return _ind; }
+	inline int next() const { return (_ind + 1) % 2; }
+	inline int prev() const { return next(); }
+	inline void incr() { _ind = (_ind + 1) % 2; }
+};
 
 // Main ////////////////////////////////////////////////////////
 int MAIN() {
@@ -37,6 +46,7 @@ int MAIN() {
 int TEST() {
 	// TEST_MAIN(R"()", R"()");
 
+	return 0;
 }
 #endif // _TESTING
 
